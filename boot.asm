0b0bbe2e32b343f66bff4e2980d9595001a6a016
StefanSkeletrix <stefanstinca0@gmail.com>
2025-07-27 16:17:13 +0100

    IslandBoot Alpha 1.0 ; basic HelloWorld bootloader

---
+++ boot.asm
@@ -0,0 +1,23 @@
+; boot.asm, a rudimentary bootloader for TeideOS
+
+BITS 16                 ; 16 bit ASM code
+ORG 0x7C00              ; BIOS charges this code at 0x7C00 after detecting MBR in disk
+
+start:
+   mov si, message      ; pointer to the message
+
+.print_loop:
+   lodsb                ; loads byte pointed by si, increments al
+   cmp al, 0            ; null terminator
+   je .hang             ; hang if string terminated
+
+
+.hang:
+   jmp .hang            ; infinite loop to avoid crashes
+
+
+
+message db "TeideOS - Alpha 0.1", 0    ; message to show
+
+times 510 - ($ - $$) db 0        ; making the bootloader 510 bytes
+dw 0xAA55                        ; MBR signature
