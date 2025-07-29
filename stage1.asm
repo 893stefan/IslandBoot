202d5659fc62d70122c9a08c05b4f180d715bc52
StefanSkeletrix <stefanstinca0@gmail.com>
2025-07-29 14:00:36 +0100

    Code actually prints message to TTY now, using call to BIOS.

--- stage1.asm
+++ stage1.asm
@@ -3,21 +3,24 @@
 BITS 16                 ; 16 bit ASM code
 ORG 0x7C00              ; BIOS charges this code at 0x7C00 after detecting MBR in disk
 
-start:
-   mov si, message      ; moves msg to the pointer
+mov si, message         ; moves message to the pointer
+call PrintStr           ; calls our function to print message
+jmp $
+PrintStr:
+    mov ah, 0x0E        ; initializing syscall (BIOS-call, technically) for printing strings on TTY
+    mov al, [si]        ; setting al to the value of si, to establish further comparison
+    loop:
+        int 0x10        ; interruption, triggers our syscall -> prints char to the TTY
+        inc si          ; increment pointer to our string
+        mov al, [si]    ; update value of al
+        cmp al, 0
+        jne loop        ; if (al == 0) we break out of the loop
+    ret
+ret
 
-.print_loop:
-   lodsb                ; loads byte pointed by si, increments al
-   cmp al, 0            ; null terminator
-   je .hang             ; hang if string terminated
+data:
+   message db "IslandBoot - Alpha 1.0", 0    ; message to show
 
-
-.hang:
-   jmp .hang            ; infinite loop to avoid crashes
-
-
-
-message db "IslandBoot - Alpha 1.0", 0    ; message to show
 
 times 510 - ($ - $$) db 0        ; making the bootloader 510 bytes
 dw 0xAA55                        ; MBR end signature
